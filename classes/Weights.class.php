0c8dc5ba4d5dbf8bf516ee53c5255557840c3d20
FlorianBrinkmann <mail@flobri.de>
2015-06-12 15:56:36 +0200

    12.06.

--- classes/Weights.class.php
+++ classes/Weights.class.php
@@ -1,39 +1,1 @@
-<?php
-class Weights {
-	var $karosserieID;
-	var $frontDoors;
-	var $rearDoors;
-	var $hood;
-	var $tailgate;
-	var $frontFenders;
-	var $hinges;
-	var $fuelFlap;
-	var $frontEndModules;
-	var $totalWeight;
-
-	function getObjectValues() {
-		$objectValues = [
-			'frontDoors',
-			'rearDoors',
-			'hood',
-			'tailgate',
-			'frontFenders',
-			'hinges',
-			'fuelFlap',
-			'frontEndModules',
-			'totalWeight'
-		];
-
-		return $objectValues;
-	}
-
-	function getValues( $wantedValues, $restArray ) {
-		$db     = new Datenbank();
-		$dbh    = $db->connect();
-		$result = $db->select( $dbh );
-		$values = new FilterArray();
-		$values = $values->filter( $result, $restArray );
-
-		return [ 'values' => $values, 'wantedValues' => $wantedValues ];
-	}
-}
+<?phpclass Weights {	var $karosserieID;	var $frontDoors;	var $rearDoors;	var $hood;	var $tailgate;	var $frontFenders;	var $hinges;	var $fuelFlap;	var $frontEndModules;	var $totalWeight;	function getObjectValues() {		$objectValues = [			'frontDoors',			'rearDoors',			'hood',			'tailgate',			'frontFenders',			'hinges',			'fuelFlap',			'frontEndModules',			'totalWeight'		];		return $objectValues;	}	function getValues( $wantedValues, $restArray ) {		$db     = new Datenbank();		$dbh    = $db->connect();		$result = $db->select( $dbh );		$values = new FilterArray();		$values = $values->filter( $result, $restArray );		return [ 'values' => $values, 'wantedValues' => $wantedValues ];	}}
