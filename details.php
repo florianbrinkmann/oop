8bfb3fccc138a238981ba6c30aeaf13b97321e92
FlorianBrinkmann <mail@flobri.de>
2015-06-12 15:58:11 +0200

    12.06.

--- details.php
+++ details.php
@@ -1,51 +1,1 @@
-<!DOCTYPE html>
-<head>
-	<meta charset="UTF-8">
-</head>
-<body>
-<?php
-require_once ( 'classes/Karosserie.class.php' );
-
-if ( isset( $_POST ) && ! empty( $_POST ) ) {
-	$karosserie   = new Karosserie();
-	$wantedValues = [
-		'oemBrand',
-		'carModel',
-		'modelYear',
-		'euroCarSegment',
-		'length',
-		'width',
-		'height',
-		'trackFront',
-		'trackRear',
-		'trackMean',
-		'wheelbase',
-		'contactArea',
-		'frontDoors',
-		'rearDoors',
-		'hood',
-		'tailgate',
-		'frontFenders',
-		'hinges',
-		'fuelFlap',
-		'frontEndModules',
-		'totalWeight',
-		'process',
-		'reTooling',
-		'degreeOfMechanisation',
-		'intendedProductionVolume',
-		'carBody',
-		'partsInTheBodyInWhite',
-		'totalNumberOfParts',
-		'numberOfPartsInWeightClassLessThan100g',
-		'numberOfPartsInWeightClass100gTo1kg',
-		'numberOfPartsInWeightClass1kgTo5kg',
-		'numberOfPartsInWeightClassMoreThan5kg'
-	];
-	$karosserien  = $karosserie->getValues( $wantedValues );
-	$display      = new Anzeige();
-	$display->detailView( $karosserien );
-
-} ?>
-</body>
-</html>
+<!DOCTYPE html><head>	<meta charset="UTF-8"></head><body><?phprequire_once ( 'classes/Karosserie.class.php' );if ( isset( $_POST ) && ! empty( $_POST ) ) {	$karosserie   = new Karosserie();	$wantedValues = [		'oemBrand',		'carModel',		'modelYear',		'euroCarSegment',		'length',		'width',		'height',		'trackFront',		'trackRear',		'trackMean',		'wheelbase',		'contactArea',		'frontDoors',		'rearDoors',		'hood',		'tailgate',		'frontFenders',		'hinges',		'fuelFlap',		'frontEndModules',		'totalWeight',		'process',		'reTooling',		'degreeOfMechanisation',		'intendedProductionVolume',		'carBody',		'partsInTheBodyInWhite',		'totalNumberOfParts',		'numberOfPartsInWeightClassLessThan100g',		'numberOfPartsInWeightClass100gTo1kg',		'numberOfPartsInWeightClass1kgTo5kg',		'numberOfPartsInWeightClassMoreThan5kg'	];	$karosserien  = $karosserie->getValues( $wantedValues );	$display      = new Anzeige();	$display->detailView( $karosserien );} ?></body></html>
