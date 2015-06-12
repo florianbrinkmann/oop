cd0cdb92f2bb42a7803895e172498063e2e7e9b7
FlorianBrinkmann <mail@flobri.de>
2015-06-12 15:56:05 +0200

    12.06.

--- classes/OEMbrand.class.php
+++ classes/OEMbrand.class.php
@@ -1,22 +1,1 @@
-<?php
-class OEMbrand {
-	var $brandID;
-	var $oemBrand;
-
-	function getObjectValues() {
-		$objectValues = [ 'oemBrand' ];
-
-		return $objectValues;
-	}
-
-	function getValues( $wantedValues, $restArray ) {
-		$db     = new Datenbank();
-		$dbh    = $db->connect();
-		$result = $db->select( $dbh );
-
-		$values = new FilterArray();
-		$values = $values->filter( $result, $restArray );
-
-		return [ 'values' => $values, 'wantedValues' => $wantedValues ];
-	}
-}
+<?phpclass OEMbrand {	var $brandID;	var $oemBrand;	function getObjectValues() {		$objectValues = [ 'oemBrand' ];		return $objectValues;	}	function getValues( $wantedValues, $restArray ) {		$db     = new Datenbank();		$dbh    = $db->connect();		$result = $db->select( $dbh );		$values = new FilterArray();		$values = $values->filter( $result, $restArray );		return [ 'values' => $values, 'wantedValues' => $wantedValues ];	}}
